1b4c44614d5eb2af167cfe5df8a97dffb13b1489
Poojan Vachharajani <pjmathematician@gmail.com>
2022-03-07 17:06:53 +0530

    basic login system with api access

---
+++ src/components/App/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from "../Dashboard/Dashboard";
+import Login from "../Login/Login";
+import Preferences from "../Preferences/Preferences";
+import useToken from "./useToken";
+
+function App() {
+    const {token, setToken} = useToken();
+    if (!token) {
+        return (
+            <Login setToken={setToken} />
+        );
+    }
+    return (
+        <div className="wrapper">
+            <h1>Application</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/preferences" element={<Preferences />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default App;
